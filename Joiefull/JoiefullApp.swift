9d82dd5376d8247a78020368c7c8207f73c332b3
TristanGeh <109686323+TristanGeh@users.noreply.github.com>
2024-09-09 09:36:08 +0200

    Initial Commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Tristan Géhanne on 09/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
